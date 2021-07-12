b823e5badabe5a86780f05ba4e4292f8d5e81a83
fahad.qureshi <fahad.qureshi@i2p.com.pk>
2021-07-12 21:56:46 +0500

    connect multi files uploader with mysql database server

--- src/main/java/com/bezkoder/spring/files/uploadmultiple/service/FilesStorageServiceImpl.java
+++ src/main/java/com/bezkoder/spring/files/uploadmultiple/service/FilesStorageServiceImpl.java
@@ -7,16 +7,24 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.stream.Stream;
 
+import com.bezkoder.spring.files.uploadmultiple.controller.FilesController;
+import com.bezkoder.spring.files.uploadmultiple.model.FileInfo;
+import com.bezkoder.spring.files.uploadmultiple.repo.FileRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.UrlResource;
 import org.springframework.stereotype.Service;
 import org.springframework.util.FileSystemUtils;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
 
 @Service
 public class FilesStorageServiceImpl implements FilesStorageService {
 
   private final Path root = Paths.get("uploads");
+
+  @Autowired
+  private FileRepository fileRepository;
 
   @Override
   public void init() {
@@ -31,6 +39,14 @@ public class FilesStorageServiceImpl implements FilesStorageService {
   public void save(MultipartFile file) {
     try {
       Files.copy(file.getInputStream(), this.root.resolve(file.getOriginalFilename()));
+      FileInfo fileInfo = new FileInfo();
+      fileInfo.setName(file.getOriginalFilename());
+      String url = MvcUriComponentsBuilder
+              .fromMethodName(FilesController.class, "getFile", file.getOriginalFilename().toString()).build().toString();
+      fileInfo.setUrl(url);
+      fileInfo.setPath(root+"/"+file.getOriginalFilename());
+      fileRepository.save(fileInfo);
+
     } catch (Exception e) {
       throw new RuntimeException("Could not store the file. Error: " + e.getMessage());
     }
